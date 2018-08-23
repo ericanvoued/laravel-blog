da15f0469bc1b459231f02e91b4f386f6568df27
ericanvoued <ericanvoued@gmail.com>
2018-08-23 20:07:21 +0800

    'luoban'

--- app/Http/Controllers/admin/CategoryController.php
+++ app/Http/Controllers/admin/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Http\Model\Category;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\admin\CommonController;
@@ -10,7 +11,27 @@ class CategoryController extends CommonController
 {
     //get admin/category  全部分类
     public function index(){
-        echo 'get';
+        $categorys = Category::all();
+        $data = $this->getTree($categorys);
+        return view('admin.list')->with('data',$data);
+    }
+
+    //分类排序
+    public function getTree($data)
+    {
+        $arr = array();
+        foreach ($data as $k => $v){
+            if($v->cate_pid == 0){
+                $arr[] = $data[$k];
+                foreach ($data as $m => $n){
+                    if($n->cate_pid == $v->cate_id){
+                        $arr[] = $data[$m];
+                    }
+                }
+            }
+        }
+        return $arr;
+//        dd($arr);
     }
 
     //post admin/category
--- resources/views/admin/list.blade.php
+++ resources/views/admin/list.blade.php
@@ -1,17 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-    <link rel="stylesheet" href="{{asset('resources/views/admin/style/css/ch-ui.admin.css')}}">
-    <link rel="stylesheet" href="{{asset('resources/views/admin/style/font/css/font-awesome.min.css')}}">
-    <script type="text/javascript" src="{{asset('resources/views/admin/style/js/jquery.js')}}"></script>
-    <script type="text/javascript" src="{{asset('resources/views/admin/style/js/ch-ui.admin.js')}}"></script>
-</head>
-<body>
+@extends('layout.admin')
+@section('content')
     <!--面包屑导航 开始-->
     <div class="crumb_warp">
         <!--<i class="fa fa-bell"></i> 欢迎使用登陆网站后台，建站的首选工具。-->
-        <i class="fa fa-home"></i> <a href="#">首页</a> &raquo; <a href="#">商品管理</a> &raquo; 添加商品
+        <i class="fa fa-home"></i> <a href="{{url('admin/info')}}">首页</a> &raquo;  全部分类
     </div>
     <!--面包屑导航 结束-->
 
@@ -55,76 +47,75 @@
             <div class="result_content">
                 <table class="list_tab">
                     <tr>
-                        <th class="tc" width="5%"><input type="checkbox" name=""></th>
                         <th class="tc">排序</th>
                         <th class="tc">ID</th>
                         <th>标题</th>
-                        <th>审核状态</th>
+                        <th>分类名称</th>
                         <th>点击</th>
                         <th>发布人</th>
                         <th>更新时间</th>
                         <th>评论</th>
                         <th>操作</th>
                     </tr>
+                    @foreach($data as $item)
                     <tr>
-                        <td class="tc"><input type="checkbox" name="id[]" value="59"></td>
                         <td class="tc">
-                            <input type="text" name="ord[]" value="0">
+                            <input type="text" name="ord[]" value="{{$item->cate_order}}">
                         </td>
-                        <td class="tc">59</td>
+                        <td class="tc">{{$item->cate_id}}</td>
                         <td>
-                            <a href="#">Apple iPhone 6 Plus (A1524) 16GB 金色 移动联通电信4G手机</a>
+                            <a href="#">{{$item->cate_title}}</a>
                         </td>
-                        <td>0</td>
-                        <td>2</td>
+                        <td> {{$item->cate_name}}</td>
+                        <td>{{$item->cate_view}}</td>
                         <td>admin</td>
-                        <td>2014-03-15 21:11:01</td>
+                        <td>{{$item->update_at}}</td>
                         <td></td>
                         <td>
                             <a href="#">修改</a>
                             <a href="#">删除</a>
                         </td>
                     </tr>
-                    
-                    <tr>
-                        <td class="tc"><input type="checkbox" name="id[]" value="59"></td>
-                        <td class="tc">
-                            <input type="text" name="ord[]" value="0">
-                        </td>
-                        <td class="tc">59</td>
-                        <td>
-                            <a href="#">三星 SM-G5308W 白色 移动4G手机 双卡双待</a>
-                        </td>
-                        <td>0</td>
-                        <td>2</td>
-                        <td>admin</td>
-                        <td>2014-03-15 21:11:01</td>
-                        <td></td>
-                        <td>
-                            <a href="#">修改</a>
-                            <a href="#">删除</a>
-                        </td>
-                    </tr>
+                    @endforeach
+                    {{--<tr>--}}
+                        {{--<td class="tc"><input type="checkbox" name="id[]" value="59"></td>--}}
+                        {{--<td class="tc">--}}
+                            {{--<input type="text" name="ord[]" value="0">--}}
+                        {{--</td>--}}
+                        {{--<td class="tc">59</td>--}}
+                        {{--<td>--}}
+                            {{--<a href="#">三星 SM-G5308W 白色 移动4G手机 双卡双待</a>--}}
+                        {{--</td>--}}
+                        {{--<td>0</td>--}}
+                        {{--<td>2</td>--}}
+                        {{--<td>admin</td>--}}
+                        {{--<td>2014-03-15 21:11:01</td>--}}
+                        {{--<td></td>--}}
+                        {{--<td>--}}
+                            {{--<a href="#">修改</a>--}}
+                            {{--<a href="#">删除</a>--}}
+                        {{--</td>--}}
+                    {{--</tr>--}}
 
-                    <tr>
-                        <td class="tc"><input type="checkbox" name="id[]" value="59"></td>
-                        <td class="tc">
-                            <input type="text" name="ord[]" value="0">
-                        </td>
-                        <td class="tc">59</td>
-                        <td>
-                            <a href="#">荣耀 6 (H60-L11) 3GB内存增强版 白色 移动4G手机</a>
-                        </td>
-                        <td>0</td>
-                        <td>2</td>
-                        <td>admin</td>
-                        <td>2014-03-15 21:11:01</td>
-                        <td></td>
-                        <td>
-                            <a href="#">修改</a>
-                            <a href="#">删除</a>
-                        </td>
-                    </tr>
+                    {{--<tr>--}}
+                        {{--<td class="tc"><input type="checkbox" name="id[]" value="59"></td>--}}
+                        {{--<td class="tc">--}}
+                            {{--<input type="text" name="ord[]" value="0">--}}
+                        {{--</td>--}}
+                        {{--<td class="tc">59</td>--}}
+                        {{--<td>--}}
+                            {{--<a href="#">荣耀 6 (H60-L11) 3GB内存增强版 白色 移动4G手机</a>--}}
+                        {{--</td>--}}
+                        {{--<td>0</td>--}}
+                        {{--<td>2</td>--}}
+                        {{--<td>admin</td>--}}
+                        {{--<td>2014-03-15 21:11:01</td>--}}
+                        {{--<td></td>--}}
+                        {{--<td>--}}
+                            {{--<a href="#">修改</a>--}}
+                            {{--<a href="#">删除</a>--}}
+                        {{--</td>--}}
+                    {{--</tr>--}}
                 </table>
 
 
@@ -162,6 +153,4 @@
     <!--搜索结果页面 列表 结束-->
 
 
-
-</body>
-</html>
+@endsection
